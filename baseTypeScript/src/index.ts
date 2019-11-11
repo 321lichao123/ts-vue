4976b937af0ddd37a2e0f82274b6ef8a259b655a
lichao <lichao_lc123@126.com>
2019-11-11 22:16:17 +0800

    tsconfig.json文件的解读

--- baseTypeScript/src/index.ts
+++ baseTypeScript/src/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2019-10-27 09:23:06
- * @LastEditTime: 2019-11-08 01:00:43
+ * @LastEditTime: 2019-11-11 21:59:16
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue+ts\baseTypeScript\src\demo1.1.ts
@@ -15,3 +15,16 @@ import "./generics";
 import "./advanced";
 let hello: string = "hello typescript";
 document.querySelectorAll(".app")[0].innerHTML = hello;
+
+// console.log([1, 2, [3, 4]].flat());
+
+class A {
+  a: number = 1;
+  getA() {
+    return () => {
+      console.log(this.a);
+    }
+  }
+}
+
+let aa = new A().getA();
